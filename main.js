ce3c181708fbe213667aac82211a2889630ad817
Max Chechel <maksymchechel@yahoo.com>
2024-06-06 16:41:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -245,111 +245,108 @@ const sliderCards = slider.querySelectorAll(
 
 let mm = gsap.matchMedia();
 
-mm.add('(min-width: 992px)', () => {
-    gsap.set(sliderCards, {
-        scale: 0.9,
-        opacity: 0.3,
-    });
+//mm.add('(min-width: 992px)', () => {
+gsap.set(sliderCards, {
+    scale: 0.9,
+    opacity: 0.3,
+});
 
-    function getScrollAmount() {
-        let racesWidth = slider.scrollWidth;
-        return racesWidth - window.innerWidth;
-    }
-    let scrollGap = window.innerWidth * 0.65;
+function getScrollAmount() {
+    let racesWidth = slider.scrollWidth;
+    return racesWidth - window.innerWidth;
+}
+let scrollGap = window.innerWidth * 0.65;
 
-    const horizontalScrollTween = gsap.to(slider, {
-        x: () => -slider.scrollWidth + scrollGap,
-        ease: 'none',
-    });
+const horizontalScrollTween = gsap.to(slider, {
+    x: () => -slider.scrollWidth + scrollGap,
+    ease: 'none',
+});
 
-    ScrollTrigger.create({
-        trigger: '.decentralization_slider-wrap',
-        start: 'top 0',
-        end: () => `+=${getScrollAmount()}`,
-        animation: horizontalScrollTween,
-        scrub: 1.5,
-        invalidateOnRefresh: true,
-        pin: true,
-    });
+ScrollTrigger.create({
+    trigger: '.decentralization_slider-wrap',
+    start: 'top 0',
+    end: () => `+=${getScrollAmount()}`,
+    animation: horizontalScrollTween,
+    scrub: 1.5,
+    invalidateOnRefresh: true,
+    pin: true,
+});
 
-    ScrollTrigger.create({
-        trigger: '.horizontal-scroll_track',
-        start: 'top 50%',
-        end: 'top 0%',
-        invalidateOnRefresh: true,
-        onEnter: () => {
-            evaluateAnimation();
-        },
-    });
+ScrollTrigger.create({
+    trigger: '.horizontal-scroll_track',
+    start: 'top 50%',
+    end: 'top 0%',
+    invalidateOnRefresh: true,
+    onEnter: () => {
+        evaluateAnimation();
+    },
+});
 
-    sliderCards.forEach((slide, index) => {
-        const tl = gsap.timeline({
-            scrollTrigger: {
-                scrub: 1.5,
-                containerAnimation: horizontalScrollTween,
-                trigger: slide,
-                start: 'left 50%',
-                end: 'right 100%',
-                toggleActions: 'play none none reverse',
-                invalidateOnRefresh: true,
-                onEnter: () => {
-                    if (
-                        slide.getAttribute('data-animation-card') ===
-                        'contribute'
-                    )
-                        contributeAnimation();
-                    if (slide.getAttribute('data-animation-card') === 'vector')
-                        vectorStorageAnimation();
-                    if (slide.getAttribute('data-animation-card') === 'compute')
-                        computeAnimation();
-                },
+sliderCards.forEach((slide, index) => {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            scrub: 1.5,
+            containerAnimation: horizontalScrollTween,
+            trigger: slide,
+            start: 'left 50%',
+            end: 'right 100%',
+            toggleActions: 'play none none reverse',
+            invalidateOnRefresh: true,
+            onEnter: () => {
+                if (slide.getAttribute('data-animation-card') === 'contribute')
+                    contributeAnimation();
+                if (slide.getAttribute('data-animation-card') === 'vector')
+                    vectorStorageAnimation();
+                if (slide.getAttribute('data-animation-card') === 'compute')
+                    computeAnimation();
             },
-        });
+        },
+    });
 
-        tl.to(slide, {
-            scale: 1,
-            opacity: 1,
-        });
+    tl.to(slide, {
+        scale: 1,
+        opacity: 1,
     });
 });
-mm.add('(max-width: 991px)', () => {
-    ScrollTrigger.create({
-        trigger: "[data-animation-card='evaluate']",
-        start: 'top 50%',
-        end: 'top 0%',
-        invalidateOnRefresh: true,
-        onEnter: () => {
-            evaluateAnimation();
-        },
-    });
-    ScrollTrigger.create({
-        trigger: "[data-animation-card='contribute']",
-        start: 'top 50%',
-        end: 'top 0%',
-        invalidateOnRefresh: true,
-        onEnter: () => {
-            contributeAnimation();
-        },
-    });
-    ScrollTrigger.create({
-        trigger: "[data-animation-card='vector']",
-        start: 'top 50%',
-        end: 'top 0%',
-        invalidateOnRefresh: true,
-        onEnter: () => {
-            vectorStorageAnimation();
-        },
-    });
-    ScrollTrigger.create({
-        trigger: "[data-animation-card='compute']",
-        start: 'top 50%',
-        end: 'top 0%',
-        invalidateOnRefresh: true,
-        onEnter: () => {
-            computeAnimation();
-        },
-    });
+//});
+//mm.add('(max-width: 991px)', () => {
+ScrollTrigger.create({
+    trigger: "[data-animation-card='evaluate']",
+    start: 'top 50%',
+    end: 'top 0%',
+    invalidateOnRefresh: true,
+    onEnter: () => {
+        evaluateAnimation();
+    },
 });
+ScrollTrigger.create({
+    trigger: "[data-animation-card='contribute']",
+    start: 'top 50%',
+    end: 'top 0%',
+    invalidateOnRefresh: true,
+    onEnter: () => {
+        contributeAnimation();
+    },
+});
+ScrollTrigger.create({
+    trigger: "[data-animation-card='vector']",
+    start: 'top 50%',
+    end: 'top 0%',
+    invalidateOnRefresh: true,
+    onEnter: () => {
+        vectorStorageAnimation();
+    },
+});
+ScrollTrigger.create({
+    trigger: "[data-animation-card='compute']",
+    start: 'top 50%',
+    end: 'top 0%',
+    invalidateOnRefresh: true,
+    onEnter: () => {
+        computeAnimation();
+    },
+});
+//});
 ////CHAT///////
 const mainChatTl = chatAnimation();
 ScrollTrigger.create({
