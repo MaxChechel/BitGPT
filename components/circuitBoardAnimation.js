acfe98d19e60fbcbeaafde226c33349159e24889
Max Chechel <maksymchechel@yahoo.com>
2024-06-11 21:13:24 +0200

    Update circuitBoardAnimation.js

--- components/circuitBoardAnimation.js
+++ components/circuitBoardAnimation.js
@@ -5,15 +5,14 @@ gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin);
 
 export default function circuitBoardAnimation(pathsSelector) {
     const circuitPaths = document.querySelectorAll(pathsSelector);
-    const circuitPathsTweens = [];
+    const masterTimeline = gsap.timeline();
 
     circuitPaths.forEach((svg) => {
         const pathLength = svg.getTotalLength();
-        const timeToPlay = Math.random() * 1 + pathLength / 20;
-        const timeDelay = Math.random() * 5 + 0.5;
+        const timeToPlay = Math.random() * 1 + pathLength / 40;
+
         const tween = gsap.timeline({
-            delay: timeDelay,
-            repeatDelay: timeDelay * 2,
+            defaults: {},
             repeat: -1,
             duration: timeToPlay,
         });
@@ -34,11 +33,8 @@ export default function circuitBoardAnimation(pathsSelector) {
                 drawSVG: '100% 100%',
                 ease: 'circ.out',
             });
-        circuitPathsTweens.push(tween);
-    });
 
-    const masterCircuitTween = gsap.timeline();
-    circuitPathsTweens.forEach((tween) => {
-        masterCircuitTween.add(tween, 0);
+        // Add each tween to the master timeline, starting at the same time
+        masterTimeline.add(tween, 0);
     });
 }
